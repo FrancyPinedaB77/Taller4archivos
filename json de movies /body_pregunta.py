d2e11fd5d7cd3fd6400b4d3a80c925574c5c1f45
FrancyPinedaB77 <francypineda77@gmail.com>
2016-11-20 15:18:20 -0500

    trae el body de  la pregunta desde Junio de 2016

--- json de movies /body_pregunta.py
+++ json de movies /body_pregunta.py
@@ -13,11 +13,11 @@ import json
 cliente = MongoClient()#Inicializar objeto
 cliente = MongoClient('127.0.0.1', 27017)#Indicar parametros del servidor
 bd = cliente.taller4 #Seleccionar Schema
-coleccion = bd.preguntas2  #Seleccionar Coleccion
+coleccion = bd.body_pregunta  #Seleccionar Coleccion
 
-x=3999
-while x <=6000:
-    c= "https://api.stackexchange.com/2.2/questions/"+str(x)+"/answers?order=desc&sort=activity&site=movies.stackexchange&filter=!2.dt38jGSq_JqOgGGwIVH&impose_throttling%20=True$throttle_stop%20=%20False&key=)F1QD2RiHCaRVYYb5yc3cw(("
+x=55000 # desde 5 Noviembre
+while x <=64000:
+    c= "https://api.stackexchange.com/2.1/questions/"+str(x)+"?site=movies.stackexchange&filter=withbody&key=)F1QD2RiHCaRVYYb5yc3cw(("
     x=x+1
     resp = requests.get(url=c)
     data = json.loads(resp.text)
@@ -25,12 +25,3 @@ while x <=6000:
         #print (data)
         coleccion.insert_one(data)
 
-
-
-
-
-#print (data['items'])
-#print (len(data['items']))
-    #print str(data)
-	
-    #coleccion.insert_one(data)
