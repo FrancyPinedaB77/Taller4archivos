421b14fb23d7f28dbd462c2b31d0febd712f87af
FrancyPinedaB77 <francypineda77@gmail.com>
2016-11-22 23:28:07 -0500

    llena las entidades de cada pregunta en la base de datos de ejemplo

--- entidades/entidades.py
+++ entidades/entidades.py
@@ -17,23 +17,23 @@ coleccion = bd.ejemploborrar  #Seleccionar Coleccion
 st = StanfordNERTagger('/home/xubuntu/Taller4/nueva/classifiers/english.muc.7class.distsim.crf.ser.gz',
 				   '/home/xubuntu/Taller4/nueva/stanford-ner.jar',encoding='utf-8')
 
-text = '  loves his country Bogota ,,she will traveled 28 October,  she study in the unversity in Colombia win $8000000 ,she will traveled 28 October, she has $32000'
-
-tokenized_text = word_tokenize(text)
-classified_text = st.tag(tokenized_text)
-array=classified_text
-
 i=0
-user=0
+preguntas=0
 #-----parametros de Consultas en mongo------#
-consulta1= coleccion.find({},{"items.body":1,"_id":0}) 
-for user in consulta1: 			#Llega la consulta
-	c = user.get("items")		#llega el body	
-	id_pregunta=user.get("id_question")	
+consulta1= coleccion.find()
+
+for preguntas in consulta1: 			#Llega la consulta
+	'''
+	print preguntas.get("items")[0]["body"]
+	print  "\n"
+        print preguntas.get("items")[0]["question_id"]
+	print  "\n"
+	'''
+	c= preguntas.get("items")[0]["body"]
+        id_pregunta=int(preguntas.get("items")[0]["question_id"])
+	#print id_pregunta
 	c=str(c)			#lo paso a string
 	c= c.replace("'"," ")		#le quitan las comillas que generan error
-#	print c
-#	print id_pregunta
 	tokenized_text = word_tokenize(c) #se separa por palabras
 	classified_text = st.tag(tokenized_text) #se usa el clasificador 
 	array=classified_text						#aca trae el body de la pregunta separadas las palabras 
@@ -52,8 +52,8 @@ for user in consulta1: 			#Llega la consulta
 		    entidades.append(t)
         entidades=json.loads(json.dumps(dict(entidades)))
 	print entidades
-        #coleccion.update({"items.question_id":104},{"$set":{"entidades":entidades}},multi=True)
-        coleccion.update({},{"$set":{"entidades":entidades}},multi=True)
+        coleccion.update({"items.question_id":id_pregunta},{"$set":{"entidades":entidades}},multi=True)
+        #coleccion.update({},{"$set":{"entidades":entidades}},multi=True)
 
 
 
