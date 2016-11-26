1cb0acc0a1b2f023da9b59efc9b5917e3217be6b
FrancyPinedaB77 <francypineda77@gmail.com>
2016-11-25 22:18:23 -0500

    trae las localizaciones por latitud y longitud y texto

--- dbpedia/ejemplodbpedia.py
+++ dbpedia/ejemplodbpedia.py
@@ -21,14 +21,16 @@ sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
 #********************************************************+
 #Trae los valores de latitud 
+
 sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?lat
-    WHERE { <http://dbpedia.org/resource/France> geo:lat ?lat }
+    WHERE { <http://dbpedia.org/resource/India> geo:lat ?lat }
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 for result in results["results"]["bindings"]:
+    print len(result)
     lat=(result["lat"]["value"])    
 print "esta es la latitut"
 print lat
@@ -36,7 +38,7 @@ print lat
 sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?long
-    WHERE { <http://dbpedia.org/resource/France> geo:long ?long }
+    WHERE { <http://dbpedia.org/resource/India> geo:long ?long }
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
@@ -46,13 +48,3 @@ for result in results["results"]["bindings"]:
 print "esta es la longitud"   
 print longit
 
-
-#Trae el tipo de localizacion 
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?comment
-    WHERE { <http://dbpedia.org/resource/France> rdfs:comment ?comment }
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-print results["results"]["bindings"][3]["comment"]["value"]
--- el mapa/llenando_el_listado_deLocalizacionesparaelmapa.py
+++ el mapa/llenando_el_listado_deLocalizacionesparaelmapa.py
@@ -17,7 +17,7 @@ coleccion = bd.body_pregunta #Seleccionar Coleccion
 a=0
 pregunta=0
 #-----parametros de Consultas en mongo------# respuestas en 63488
-consulta1= coleccion.find({"items.question_id":{"$gte":63526}})
+consulta1= coleccion.find({"items.question_id":{"$gte":60000}})
 #------------informacion para traer de DBPEDIA.......................
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
@@ -30,10 +30,10 @@ for pregunta in consulta1: 			#Llega la consulta
 	a=a+1
 	entidad_localizacion=entidad_localizacion.get("LOCATION")
 	if entidad_localizacion != None:
+		entidad_localizacion=entidad_localizacion.title()
 		texto_marcador= entidad_localizacion+" "+ "Pregunta #"+str(id_pregunta) + ":" + titulo_pregunta
-		print entidad_localizacion
 		lugar= entidad_localizacion
-		print lugar
+		#print lugar
 		#se busca la latitud******************* 
 		
 		#if results
@@ -45,25 +45,27 @@ for pregunta in consulta1: 			#Llega la consulta
 			""")
 			sparql.setReturnFormat(JSON)
 			results = sparql.query().convert()
-			for trae_latitud in results["results"]["bindings"]:
-				print trae_latitud
-			punto_latitud=(trae_latitud["lat"]["value"])    
-			print "esta es la latitut"
-			print punto_latitud
-			#se busca la longitud
-			sparql.setQuery("""
-			    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-			    SELECT ?long
-			    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:long ?long }
-			""")
-			sparql.setReturnFormat(JSON)
-			results = sparql.query().convert()
-			for trae_longitud in results["results"]["bindings"]:
-				a=2
-				#print (result["long"]["value"])
-			punto_longitud=(trae_longitud["long"]["value"]) 
-			print "esta es la longitud"   
-			print punto_longitud
+			for prueba_que_tenga in results["results"]["bindings"]:
+				if len(prueba_que_tenga)==1:
+					for trae_latitud in results["results"]["bindings"]:
+						punto_latitud=(trae_latitud["lat"]["value"]) 
+					print texto_marcador   
+					print "esta es la latitut" +"de" + lugar
+					print punto_latitud
+					#se busca la longitud
+					sparql.setQuery("""
+					    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+					    SELECT ?long
+					    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:long ?long }
+					""")
+					sparql.setReturnFormat(JSON)
+					results = sparql.query().convert()
+					for trae_longitud in results["results"]["bindings"]:
+						punto_longitud=(trae_longitud["long"]["value"]) 
+					print "esta es la longitud" +"de" + lugar
+					print punto_longitud
+					print "-------------------------------------------------------------"
+
 		except Exception as error:
 			print('ereerror: ' + repr(error)+"-" +lugar +":")
 
