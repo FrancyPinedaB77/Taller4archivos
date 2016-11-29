d8eca1aa201b9a75eb5bd2d8f96d51c2455fee95
FrancyPinedaB77 <francypineda77@gmail.com>
2016-11-29 13:39:38 -0500

    se hace el aprograma para incluir las entidades en la base de datos pero esta incluyendo entidades repetidas, pendiente arreglar, pero ya estan almacenadas en la base de datos

--- el mapa/entidadesparalasorganizaciones.py
+++ el mapa/entidadesparalasorganizaciones.py
@@ -26,45 +26,45 @@ localizaciones_js=[]
 for pregunta in trae_todo_coleccion: 			#Llega la consulta
 	titulo_pregunta= pregunta.get("items")[0]["title"]
 	id_pregunta=int(pregunta.get("items")[0]["question_id"])
-	entidad_organizacion=pregunta.get("entidades")
-	a=a+1
-	entidad_organizacion=entidad_organizacion.get("ORGANIZATION")
-	#print entidad_organizacion
-	if entidad_organizacion != None:
-		entidad_organizacion=entidad_organizacion.title()
-		texto_marcador= entidad_organizacion+" "+ "Pregunta #"+str(id_pregunta) + ":" + titulo_pregunta
-		lugar= entidad_organizacion
-		print lugar
-##
- 		try :
- 			sparql.setQuery("""
- 		    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
- 		    SELECT ?lat
- 		    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:lat ?lat }
- 			""")
- 			sparql.setReturnFormat(JSON)
- 			results = sparql.query().convert()
- 			for prueba_que_tenga in results["results"]["bindings"]:
- 				if len(prueba_que_tenga)==1:
- 					for trae_latitud in results["results"]["bindings"]:
- 						punto_latitud=(trae_latitud["lat"]["value"]) 
- 					sparql.setQuery("""PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
- 					    SELECT ?long
- 					    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:long ?long }
- 					""")
-					sparql.setReturnFormat(JSON)
-					results = sparql.query().convert()
-					#print results
-					for trae_longitud in results["results"]["bindings"]:
-						punto_longitud=(trae_longitud["long"]["value"]) 
+	entidad_organizacion=pregunta.get("entidades")[3]["ORGANIZATION"]
+	print entidad_organizacion
+	print len(entidad_organizacion)
+# 	a=a+1
+# 	entidad_organizacion=entidad_organizacion.get("ORGANIZATION")
+# 	if entidad_organizacion != None:
+# 		entidad_organizacion=entidad_organizacion.title()
+# 		texto_marcador= entidad_organizacion+" "+ "Pregunta #"+str(id_pregunta) + ":" + titulo_pregunta
+# 		lugar= entidad_organizacion
+# 		print lugar
+#  		try :
+#  			sparql.setQuery("""
+#  		    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#  		    SELECT ?lat
+#  		    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:lat ?lat }
+#  			""")
+#  			sparql.setReturnFormat(JSON)
+#  			results = sparql.query().convert()
+#  			for prueba_que_tenga in results["results"]["bindings"]:
+#  				if len(prueba_que_tenga)==1:
+#  					for trae_latitud in results["results"]["bindings"]:
+#  						punto_latitud=(trae_latitud["lat"]["value"]) 
+#  					sparql.setQuery("""PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#  					    SELECT ?long
+#  					    WHERE { <http://dbpedia.org/resource/"""+lugar+"""> geo:long ?long }
+#  					""")
+# 					sparql.setReturnFormat(JSON)
+# 					results = sparql.query().convert()
 
-					array_completo=[str(texto_marcador),float(punto_latitud),float(punto_longitud)]
-					print array_completo
-					localizaciones_js.append(array_completo)
-				print localizaciones_js
+# 					for trae_longitud in results["results"]["bindings"]:
+# 						punto_longitud=(trae_longitud["long"]["value"]) 
 
-		except Exception as error:
-			aa=3
+# 					array_completo=[str(texto_marcador),float(punto_latitud),float(punto_longitud)]
+# 					print array_completo
+# 					localizaciones_js.append(array_completo)
+# 				print localizaciones_js
 
-print localizaciones_js
+# 		except Exception as error:
+# 			aa=3
+
+# print localizaciones_js
 
--- entidades/completas_las_entidades.py
+++ entidades/completas_las_entidades.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
 import json 
 from bson import json_util 
@@ -10,12 +11,12 @@ from nltk.tokenize import word_tokenize
 cliente = MongoClient()#Inicializar objeto
 cliente = MongoClient('127.0.0.1', 27017)#Indicar parametros del servidor
 bd = cliente.taller4 #Seleccionar Schema
-coleccion = bd.body_pregunta  #Seleccionar Coleccion
+coleccion = bd.body_respuestas  #Seleccionar Coleccion
 
 st = StanfordNERTagger('/home/xubuntu/Taller4/nueva/classifiers/english.muc.7class.distsim.crf.ser.gz',
 	'/home/xubuntu/Taller4/nueva/stanford-ner.jar',encoding='utf-8')
 
-consulta1= coleccion.find({"items.question_id":{"$gte":63724}})
+#consulta1= coleccion.find({"items.question_id":{"$gte":60000}})
 pregunta=0
 
 p=0
@@ -28,10 +29,11 @@ entidades = []
 try:
 	bd = cliente.taller4 #Seleccionar Schema
 	coleccion = bd.body_respuestas  #Seleccionar Coleccion
-	consulta1= coleccion.find({"items.question_id":{"$gte":63724}})
+	consulta1= coleccion.find({"items.question_id":{"$gte":60000}})
 	cliente.close()
 	for pregunta in consulta1:
 		c= pregunta.get("items")[0]["body"]
+		id_pregunta=int(pregunta.get("items")[0]["question_id"])
 		tokenized_text = word_tokenize(c)
 		classified_text = st.tag(tokenized_text)
 		entidades_personas=[]
@@ -39,51 +41,58 @@ try:
 		entidades_date=[]
 		entidades_organization=[]
 		entidades_money=[]
-		entidades_percent=[]
+		entidades_time=[]
 		entidades=[]
 		checker=True
 		checked=True
 		for z in range (len(classified_text)):		
 			if classified_text[z][1]=="PERSON" :
 				if classified_text[z+1][1]=="PERSON" :
-					entidades_personas.append({p:str(classified_text[z][0]) + " " + str(classified_text[z+1][0])})	
+					entidades_personas.append({"p"+str(p):str(classified_text[z][0]) + " " + str(classified_text[z+1][0])})	
 					checker = False
+
 				else:
 					if checker:
-						entidades_personas.append({p:str(classified_text[z][0])})
+						entidades_personas.append({"p"+str(p):str(classified_text[z][0])})
 					else:
 						checker=True
 				p=p+1
 			elif classified_text[z][1]=="LOCATION":
 				if classified_text[z+1][1]=="LOCATION" :
-					entidades_ubicacion.append({y:str(classified_text[z][0]) + " " + str(classified_text[z+1][0])})	
+					entidades_ubicacion.append({"y"+str(y):str(classified_text[z][0]) + " " + str(classified_text[z+1][0])})	
 					checked= False
 				else:
 					if checked:
-						entidades_ubicacion.append({y:str(classified_text[z][0])})
+						entidades_ubicacion.append({"y"+str(y):str(classified_text[z][0])})
 					else:
 						checked=True
 				y=y+1
 			elif classified_text[z][1]=="DATE":
-				entidades_date.append({k:str(classified_text[z][0])})
+				entidades_date.append({"k"+str(k):str(classified_text[z][0])})
 				k=k+1
 			elif classified_text[z][1]=="ORGANIZATION":
 				if len(classified_text[z][0])>1:
-					entidades_organization.append({l:str(classified_text[z][0])})
+					entidades_organization.append({"l"+str(l):str(classified_text[z][0])})
 					l=l+1	
 			elif classified_text[z][1]=="MONEY":
-				entidades_money.append({m:str(classified_text[z][0])})
+				entidades_money.append({"m"+str(m):str(classified_text[z][0])})
 				m=m+1
-			elif classified_text[z][1]=="PERCENT":
-				entidades_percent.append({n:str(classified_text[z][0])})
+			elif classified_text[z][1]=="TIME":
+				entidades_time.append({"n"+str(n):str(classified_text[z][0])})
 				n=n+1
 		entidades.append({"PERSON": entidades_personas})
 		entidades.append({"LOCATION":entidades_ubicacion})
 		entidades.append({"DATE":entidades_date})
 		entidades.append({"ORGANIZATION":entidades_organization})
 		entidades.append({"MONEY":entidades_money})
-		entidades.append({"PERCENT":entidades_percent})#print "el json------------------"
-		print json.dumps(entidades)	
+		entidades.append({"TIME":entidades_time})#print "el json------------------"
+		p=y=k=l=m=n=0
+		print id_pregunta
+		json.dumps(entidades)
+		#print entidades
+
+		
+		coleccion.update({"items.question_id":id_pregunta},{"$set":{"entidades":entidades}},multi=True) 
 
 except Exception as e:
 	print e
